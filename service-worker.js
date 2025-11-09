a5908cc837ba940a34d017a1d38e64fd1cb16f6e
marcosdoval-swat <marcosdoval@gmail.com>
2025-11-09 09:46:36 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'carometro-cache-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './carometro_normalizado.json',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
